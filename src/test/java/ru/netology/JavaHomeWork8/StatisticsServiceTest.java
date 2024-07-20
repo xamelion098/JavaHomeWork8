3ce68da30b37ed494e521ae54c15ae2ca3c5f037
Aleksandr Stupnikov <a-a-stupnikov@yandex-team.ru>
2024-07-20 15:14:40 +0300

    hw8.1

--- src/test/java/ru/netology/JavaHomeWork8/StatisticsServiceTest.java
+++ src/test/java/ru/netology/JavaHomeWork8/StatisticsServiceTest.java
@@ -17,4 +17,15 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void findMaxFromMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 55, 3, 8, 6, 11, 11, 12};
+        long expected = 55;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
