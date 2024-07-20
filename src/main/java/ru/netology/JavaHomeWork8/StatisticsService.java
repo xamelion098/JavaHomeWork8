341149165e5cd450e6f43a1b710ce49c1e181ae1
Aleksandr Stupnikov <a-a-stupnikov@yandex-team.ru>
2024-07-20 14:41:16 +0300

    init project

---
+++ src/main/java/ru/netology/JavaHomeWork8/StatisticsService.java
@@ -0,0 +1,13 @@
+package ru.netology.JavaHomeWork8;
+
+public class StatisticsService {
+    public long findMax(long[] incomes) {
+        long currentMax = incomes[0];
+        for (long income : incomes) {
+            if (currentMax < income) {
+                currentMax = income;
+            }
+        }
+        return currentMax;
+    }
+}
